193eedf92ccfc1c4e84973b1d93e593ba6a7b888
unknown <votesdw@yahoo.com>
2021-05-31 18:47:36 +0300

    concatenation example

---
+++ BasicVariabletask.py
@@ -0,0 +1,8 @@
+#In this basic variable assignment, A student learning Python was trying to make a function.
+# His code should concatenate a passed string name with string "Edabit" and store it in a variable called result.
+# He needs your help to fix this code
+name_string = input("Enter the Passed String: ")
+print("|==============================|")
+print("You Entered: " , name_string)
+result = name_string+("Edabit")
+print("Your Result is: " + result)
